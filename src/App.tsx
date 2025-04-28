8bc4abe1ce9ac9a68e701c14ffdfd14f19062151
Erhahon Imafidon <mafiosco4@gmail.com>
2025-04-28 16:15:22 +0100

    imported the imageslider component and the images itself

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { car1, car3, car2, car4, car5 } from './assets/index';
+import ImageSlider from './components/ImageSlider.tsx';
+
+const IMAGES = [car1, car2, car3, car4, car5];
+
 const App = () => {
     return <div>Image and card slider Carousels</div>;
 };
