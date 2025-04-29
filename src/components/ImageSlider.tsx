755a37487ad299a0551456ef67d7c597812d8d6c
Erhahon Imafidon <mafiosco4@gmail.com>
2025-04-29 04:49:24 +0100

    added previous and next button and functionality

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,4 +1,57 @@
-const ImageSlider = () => {
-    return <div>ImageSlider</div>;
+import { useState } from 'react';
+import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
+
+type ImageSliderProps = {
+    imageUrls: string[];
+};
+
+const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const showNextImage = () => {
+        setImageIndex((index) => {
+            if (index === imageUrls.length - 1) {
+                return 0;
+            }
+            return index + 1;
+        });
+    };
+
+    const showPrevImage = () => {
+        setImageIndex((index) => {
+            if (index === 0) {
+                return imageUrls.length - 1;
+            }
+            return index - 1;
+        });
+    };
+
+    return (
+        <div className="w-full h-full relative">
+            <div className="w-full aspect-10/6 relative">
+                <img
+                    src={imageUrls[imageIndex]}
+                    alt={
+                        imageIndex === 0
+                            ? 'Image slider 1'
+                            : `image slider ${imageIndex + 1}`
+                    }
+                    className="object-cover w-full h-full absolute inset-0"
+                />
+                <button
+                    onClick={showPrevImage}
+                    className="absolute block  inset-y-0 p-4 cursor-pointer left-0 *:stroke-white *:fill-black *:size-8 hover:bg-black/20 transition duration-300 ease-in-out"
+                >
+                    <ArrowBigLeft />
+                </button>
+                <button
+                    onClick={showNextImage}
+                    className="absolute block inset-y-0 p-4 cursor-pointer right-0 *:stroke-white *:fill-black *:size-8 hover:bg-black/20 transition duration-300 ease-in-out"
+                >
+                    <ArrowBigRight />
+                </button>
+            </div>
+        </div>
+    );
 };
 export default ImageSlider;
