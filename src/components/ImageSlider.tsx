d80b96426797f8adf22dcf40afbd8c4d8b6fc3d8
Erhahon Imafidon <mafiosco4@gmail.com>
2025-05-01 22:48:15 +0100

    added transition animation to this component

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -29,18 +29,29 @@ const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
     return (
         <div className="w-full h-full relative">
             <div className="w-full aspect-10/6 relative">
-                <img
-                    src={imageUrls[imageIndex]}
-                    alt={
-                        imageIndex === 0
-                            ? 'Image slider 1'
-                            : `image slider ${imageIndex + 1}`
-                    }
-                    className="object-cover w-full h-full absolute inset-0"
-                />
+                <div className="w-full h-full flex overflow-hidden">
+                    {imageUrls.map((url, index) => {
+                        console.log('url', url);
+                        return (
+                            <img
+                                key={index}
+                                src={url}
+                                alt={
+                                    imageIndex === 0
+                                        ? 'Image slider 1'
+                                        : `image slider ${index + 1}`
+                                }
+                                className={`object-cover w-full h-full shrink-0 grow-0 transition-transform duration-500 ease-in-out`}
+                                style={{
+                                    translate: `${-100 * imageIndex}%`,
+                                }}
+                            />
+                        );
+                    })}
+                </div>
                 <button
                     onClick={showPrevImage}
-                    className="absolute block  inset-y-0 p-4 cursor-pointer left-0 *:stroke-white *:fill-black *:size-8 hover:bg-black/20 transition duration-300 ease-in-out"
+                    className="absolute block inset-y-0 p-4 cursor-pointer left-0 *:stroke-white *:fill-black *:size-8 hover:bg-black/20 transition duration-300 ease-in-out"
                 >
                     <ArrowBigLeft />
                 </button>
