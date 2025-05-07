474bb09e33144a167d9afc9c743b1f6d7ea86791
Erhahon Imafidon <mafiosco4@gmail.com>
2025-05-07 01:07:43 +0100

    added a fade background with before and after pseudo class

--- src/components/LogoSlide.tsx
+++ src/components/LogoSlide.tsx
@@ -21,8 +21,8 @@ const LogoSlide = () => {
         { src: Buzzfeed, alt: 'Buzzfeed' },
     ];
     return (
-        <section className="logos bg-white py-15 w-full overflow-hidden whitespace-nowrap cursor-pointer">
-            <div className="flex animate-slide logos-slide">
+        <section className="logos bg-white py-15 w-full overflow-hidden whitespace-nowrap cursor-pointer relative before:absolute before:top-0 before:left-0 before:w-60 before:h-full before:bg-gradient-to-l before:from-transparent before:to-white after:absolute after:top-0 after:right-0 after:w-60 after:h-full after:bg-gradient-to-r after:from-transparent after:to-white">
+            <div className="flex gap-x-10 animate-slide logos-slide">
                 <div className="inline-flex gap-10 sm:gap-15 shrink-0">
                     {logos.map((logo) => (
                         <img
